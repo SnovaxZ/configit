4ab42aa4b19300fd829ac329ecaa3104244f3724
SnovaxZ <gkeskikorhonen@gmail.com>
2024-06-23 21:31:04 +0300

    initial commit

---
+++ leftwm/themes/dracula/test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+export SCRIPTPATH
+SCRIPTPATH="$(
+	cd "$(dirname "$0")" || exit
+	pwd -P
+)"
+
+pkill polybar
+index=0
+leftwm-state -q -n -t "$SCRIPTPATH"/sizes.liquid | sed -r '/^\s*$/d' | while read -r width; do
+	barname="mainbar$index"
+	monitor="$(polybar -m | tac | awk -v i="$((index + 1))" 'NR==i{print}' | sed s/:.*// | tr -d '\n')"
+	echo "$monitor"
+	monitor=$monitor width=$((width - 16)) polybar -c "$SCRIPTPATH"/config.ini $barname &
+	index=$((index + 1))
+done
